b133a2c1164fe52023f74f6a94d3a8902615eabd
shimona_elora <umysharma@gmail.com>
2018-08-18 04:40:51 +0530

    Initial commit

---
+++ Assets/Scripts/AnimationSceneScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimationSceneScript : MonoBehaviour {
+
+    public Text inviteeName;
+    public Text date;
+
+	// Use this for initialization
+	void Start () {
+        inviteeName.text = GlobalControl.Instance.inviteeName;
+        date.text = GlobalControl.Instance.date;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GlobalControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalControl : MonoBehaviour
+{
+    public static GlobalControl Instance;
+
+    public string inviteeName;
+    public string date;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
