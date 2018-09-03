f00fb1650ce036236887d87884ef977227bc30f1
shimona_elora <umysharma@gmail.com>
2018-09-03 06:52:35 +0530

    Code comment

--- Assets/Scripts/RecordCamera.cs
+++ Assets/Scripts/RecordCamera.cs
@@ -11,13 +11,13 @@ public class RecordCamera : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        VideoFormat videoFormat = new VideoFormat(GetComponent<Camera>().pixelWidth, GetComponent<Camera>().pixelHeight);
-        NatCorder.StartRecording(Container.MP4, videoFormat, AudioFormat.Unity, OnRecording);
-        var videoRecorder = CameraRecorder.Create(GetComponent<Camera>());
-        videoRecorder.recordEveryNthFrame = 4;
-        var audioRecorder = AudioRecorder.Create(GetComponent<AudioListener>());
-        timeLeft = 20f;
-        StartCoroutine(startWait());
+        //VideoFormat videoFormat = new VideoFormat(GetComponent<Camera>().pixelWidth, GetComponent<Camera>().pixelHeight);
+        //NatCorder.StartRecording(Container.MP4, videoFormat, AudioFormat.Unity, OnRecording);
+        //var videoRecorder = CameraRecorder.Create(GetComponent<Camera>());
+        //videoRecorder.recordEveryNthFrame = 4;
+        //var audioRecorder = AudioRecorder.Create(GetComponent<AudioListener>());
+        //timeLeft = 20f;
+        //StartCoroutine(startWait());
     }
 
     private void OnRecording(string path)
@@ -30,26 +30,26 @@ public class RecordCamera : MonoBehaviour {
 
     //Update is called once per frame
     void Update () {
-        if (NatCorder.IsRecording /*&& cameraTexture.didUpdateThisFrame*/)
-        {
-            // Acquire an encoder frame from NatCorder
-            var frame = NatCorder.AcquireFrame();
-            // Blit the current camera preview frame to the encoder frame
-            Graphics.Blit(GetComponent<Camera>().targetTexture, frame);
-            // Commit the frame to NatCorder for encoding
-            NatCorder.CommitFrame(frame);
-        }
-        //Time.timeScale = 1;
-        if (timeLeft > 0)
-        {
-            timeLeft -= Time.deltaTime;
-            Debug.Log(timeLeft);
-        }
-        if (timeLeft < 0)
-        {
-            Debug.Log("here2");
-            //NatCorder.StopRecording();
-        }
+        //if (NatCorder.IsRecording /*&& cameraTexture.didUpdateThisFrame*/)
+        //{
+        //    // Acquire an encoder frame from NatCorder
+        //    var frame = NatCorder.AcquireFrame();
+        //    // Blit the current camera preview frame to the encoder frame
+        //    Graphics.Blit(GetComponent<Camera>().targetTexture, frame);
+        //    // Commit the frame to NatCorder for encoding
+        //    NatCorder.CommitFrame(frame);
+        //}
+        ////Time.timeScale = 1;
+        //if (timeLeft > 0)
+        //{
+        //    timeLeft -= Time.deltaTime;
+        //    Debug.Log(timeLeft);
+        //}
+        //if (timeLeft < 0)
+        //{
+        //    Debug.Log("here2");
+        //    //NatCorder.StopRecording();
+        //}
     }
 
     IEnumerator startWait()
