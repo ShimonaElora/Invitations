bdb107a62b07f4b40f06b5375ab1664dfaac75c7
shimona_elora <umysharma@gmail.com>
2018-09-04 19:46:55 +0530

    Data in animation screen add, bug fixes

--- Assets/Scripts/AnimationSceneScript.cs
+++ Assets/Scripts/AnimationSceneScript.cs
@@ -5,23 +5,54 @@ using UnityEngine.UI;
 
 public class AnimationSceneScript : MonoBehaviour {
 
-    public Text inviteeName;
-    public Text date;
+    public TMPro.TextMeshPro inviteeName;
+    public TMPro.TextMeshPro date;
+    public TMPro.TextMeshPro message;
+    public TMPro.TextMeshPro functionName;
+    public TMPro.TextMeshPro time;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         if (GlobalControl.Instance != null && GlobalControl.Instance.inviteeName != null)
         {
             inviteeName.text = GlobalControl.Instance.inviteeName;
+        }
+        else
+        {
+            inviteeName.alpha = 0;
         }
         if (GlobalControl.Instance != null && GlobalControl.Instance.date != null)
         {
             date.text = GlobalControl.Instance.date;
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        else
+        {
+            date.alpha = 0;
+        }
+        if (GlobalControl.Instance != null && GlobalControl.Instance.message != null)
+        {
+            message.text = GlobalControl.Instance.message;
+        }
+        else
+        {
+            message.alpha = 0;
+        }
+        if (GlobalControl.Instance != null && GlobalControl.Instance.functionName != null)
+        {
+            functionName.text = GlobalControl.Instance.functionName;
+        }
+        else
+        {
+            functionName.alpha = 0;
+        }
+        if (GlobalControl.Instance != null && GlobalControl.Instance.time != null)
+        {
+            time.text = GlobalControl.Instance.time;
+        }
+        else
+        {
+            time.alpha = 0;
+        }
+    }
+
 }
--- Assets/Scripts/RecordCamera.cs
+++ Assets/Scripts/RecordCamera.cs
@@ -11,13 +11,63 @@ public class RecordCamera : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        //VideoFormat videoFormat = new VideoFormat(GetComponent<Camera>().pixelWidth, GetComponent<Camera>().pixelHeight);
-        //NatCorder.StartRecording(Container.MP4, videoFormat, AudioFormat.Unity, OnRecording);
-        //var videoRecorder = CameraRecorder.Create(GetComponent<Camera>());
-        //videoRecorder.recordEveryNthFrame = 4;
-        //var audioRecorder = AudioRecorder.Create(GetComponent<AudioListener>());
-        //timeLeft = 20f;
-        //StartCoroutine(startWait());
+
+        float targetaspect = 1f;
+
+        // determine the game window's current aspect ratio
+        float windowaspect = (float)Screen.width / (float)Screen.height;
+
+        // current viewport height should be scaled by this amount
+        float scaleheight = windowaspect / targetaspect;
+
+        // obtain camera component so we can modify its viewport
+        Camera camera = GetComponent<Camera>();
+
+        Debug.Log(Screen.height + " " + camera.orthographicSize);
+
+        //camera.orthographicSize = Screen.height / 2f;
+
+        float width = camera.orthographicSize * 2f * camera.aspect;
+
+        camera.aspect = 1;
+
+        camera.orthographicSize = width / 2f;
+
+        //camera.pixelRect = new Rect(0, 0, Screen.width, Screen.width);
+
+        // if scaled height is less than current height, add letterbox
+        //if (scaleheight < 1.0f)
+        //{
+        //    Rect rect = camera.rect;
+
+        //    rect.width = scaleheight;
+        //    rect.height = 1.0f;
+        //    rect.x = 0;
+        //    rect.y = (1.0f - scaleheight) / 2.0f;
+
+        //    camera.rect = rect;
+        //}
+        //else // add pillarbox
+        //{
+        //    float scalewidth = 1.0f / scaleheight;
+
+        //    Rect rect = camera.rect;
+
+        //    rect.width = scalewidth;
+        //    rect.height = 1.0f;
+        //    rect.x = (1.0f - scalewidth) / 2.0f;
+        //    rect.y = 0;
+
+        //    camera.rect = rect;
+        //}
+
+        VideoFormat videoFormat = new VideoFormat(GetComponent<Camera>().pixelWidth, GetComponent<Camera>().pixelWidth, 60);
+        NatCorder.StartRecording(Container.MP4, videoFormat, AudioFormat.Unity, OnRecording);
+        var videoRecorder = CameraRecorder.Create(GetComponent<Camera>());
+        videoRecorder.recordEveryNthFrame = 4;
+        var audioRecorder = AudioRecorder.Create(GetComponent<AudioListener>());
+        timeLeft = 20f;
+        StartCoroutine(startWait());
     }
 
     private void OnRecording(string path)
@@ -30,26 +80,26 @@ public class RecordCamera : MonoBehaviour {
 
     //Update is called once per frame
     void Update () {
-        //if (NatCorder.IsRecording /*&& cameraTexture.didUpdateThisFrame*/)
-        //{
-        //    // Acquire an encoder frame from NatCorder
-        //    var frame = NatCorder.AcquireFrame();
-        //    // Blit the current camera preview frame to the encoder frame
-        //    Graphics.Blit(GetComponent<Camera>().targetTexture, frame);
-        //    // Commit the frame to NatCorder for encoding
-        //    NatCorder.CommitFrame(frame);
-        //}
-        ////Time.timeScale = 1;
-        //if (timeLeft > 0)
-        //{
-        //    timeLeft -= Time.deltaTime;
-        //    Debug.Log(timeLeft);
-        //}
-        //if (timeLeft < 0)
-        //{
-        //    Debug.Log("here2");
-        //    //NatCorder.StopRecording();
-        //}
+        if (NatCorder.IsRecording /*&& cameraTexture.didUpdateThisFrame*/)
+        {
+            // Acquire an encoder frame from NatCorder
+            //var frame = NatCorder.AcquireFrame();
+            // Blit the current camera preview frame to the encoder frame
+            //Graphics.Blit(GetComponent<Camera>().targetTexture, frame);
+            // Commit the frame to NatCorder for encoding
+            //NatCorder.CommitFrame(frame);
+        }
+        //Time.timeScale = 1;
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            Debug.Log(timeLeft);
+        }
+        if (timeLeft < 0)
+        {
+            Debug.Log("here2");
+            //NatCorder.StopRecording();
+        }
     }
 
     IEnumerator startWait()
